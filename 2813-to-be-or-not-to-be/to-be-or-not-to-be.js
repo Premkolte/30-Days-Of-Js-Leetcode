ef4e311837244e3754ab4b84be1631489c85722e
Prem Kolte <100456448+Premkolte@users.noreply.github.com>
2024-03-20 02:29:17 +0530

    Time: 51 ms (63.71%) | Memory: 49 MB (16.21%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return{
+        toBe:(n)=>{
+            if(val===n){
+                return true;
+            }
+            else{
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe:(n)=>{
+            if(n!==val){
+                return true;
+            }
+            else{
+                throw new Error("Equal");
+            }
+        }
+    }
+    
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
