c0f22a84167a17a21266ab6810edc2ed55373ec6
Prem Kolte <100456448+Premkolte@users.noreply.github.com>
2024-03-24 02:09:39 +0530

    Time: 58 ms (79.46%) | Memory: 49.4 MB (62.27%) - LeetSync

---
+++ 2859-add-two-promises/add-two-promises.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    const [val1,val2] = await Promise.all([promise1,promise2])
+
+    return val1+val2;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
