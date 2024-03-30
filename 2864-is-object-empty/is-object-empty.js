07ae9594b89a6037d263f67b357d430a65fda26b
Prem Kolte <100456448+Premkolte@users.noreply.github.com>
2024-03-31 02:55:15 +0530

    Time: 51 ms (61.74%) | Memory: 49.5 MB (35.18%) - LeetSync

--- 2864-is-object-empty/is-object-empty.js
+++ 2864-is-object-empty/is-object-empty.js
@@ -2,17 +2,6 @@
  * @param {Object|Array} obj
  * @return {boolean}
  */
-function isEmpty(input) {
-  // Check for object
-  if (typeof input === 'object' && !Array.isArray(input)) {
-    return Object.keys(input).length === 0;
-  }
-  
-  // Check for array
-  if (Array.isArray(input)) {
-    return input.length === 0;
-  }
-  
-  // Other data types are not considered empty
-  return false;
-}
+var isEmpty = function(obj) {
+    return !Object.keys(obj).length
+};
