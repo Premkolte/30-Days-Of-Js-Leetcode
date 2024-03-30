d4fc67670f4038de7fc652e9373214e9deac052a
Prem Kolte <100456448+Premkolte@users.noreply.github.com>
2024-03-31 02:48:57 +0530

    Time: 73 ms (20.33%) | Memory: 48.8 MB (97.09%) - LeetSync

---
+++ 2862-interval-cancellation/interval-cancellation.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    fn(...args);
+    let timer = setInterval(() => fn(...args), t);
+
+    let cancelFn = () => clearInterval(timer);
+    return cancelFn;
+};
+
