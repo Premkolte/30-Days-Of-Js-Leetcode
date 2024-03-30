613169fe8ade950c94bba55e4a9bd29cf065f46d
Prem Kolte <100456448+Premkolte@users.noreply.github.com>
2024-03-31 02:59:54 +0530

    Time: 83 ms (88.03%) | Memory: 68.7 MB (64.92%) - LeetSync

---
+++ 2759-flatten-deeply-nested-array/flatten-deeply-nested-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n) {
+    const res = [];
+    
+    function helper(arr, depth) {
+        for(const val of arr) {
+            if (typeof val === "object" && depth < n) {
+                helper(val, depth + 1);
+            } else {
+                res.push(val);
+            }
+        };
+        return res;
+    };
+
+    return helper(arr, 0);
+};
